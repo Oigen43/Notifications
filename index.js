16af3f1c298012abc1623fa11f011e837914f296
Oigen43 <45462253+Oigen43@users.noreply.github.com>
2019-08-13 23:40:44 +0300

    'add: add all types of toasts'

--- index.js
+++ index.js
@@ -1,24 +1,47 @@
-function Notification(type) {
-    this.type = type;
+function Notification() {
 }
 
 Notification.prototype.showNotification = function() {
     var notificationsList = document.querySelector('.section-notifications');
     var notification = document.createElement('div');
-    notification.className = 'info-notification';
+    notification.className = this.type;
+    notification.innerHTML = this.content;
     notificationsList.appendChild(notification);
-    alert(this.type);
 };
 
-function ToastNotification() {
-    Notification.apply(this, arguments);
+function ToastNotification(type, content) {
+    this.type = type;
+    this.content = content;
+    this.time = 5;
 };
 
 ToastNotification.prototype = Object.create(Notification.prototype);
 
 var errorButton = document.querySelector('.error-button');
+var warningButton = document.querySelector('.warning-button');
+var successButton = document.querySelector('.success-button');
+var infoButton = document.querySelector('.info-button');
 
 errorButton.onclick = function() {
-    var error = new ToastNotification('error');
+    var error = new ToastNotification('error-notification notification',
+        '<i class="fas fa-times"></i><h4>Error</h4><i class="far fa-times-circle"></i>');
     error.showNotification();
+}
+
+warningButton.onclick = function() {
+    var warning = new ToastNotification('warning-notification notification',
+        '<i class="fas fa-exclamation-triangle"></i><h4>Warning</h4><i class="far fa-times-circle"></i>');
+    warning.showNotification();
+}
+
+successButton.onclick = function() {
+    var success = new ToastNotification('success-notification notification',
+        '<i class="fas fa-check"></i><h4>Success</h4><i class="far fa-times-circle"></i>');
+    success.showNotification();
+}
+
+infoButton.onclick = function() {
+    var info = new ToastNotification('info-notification notification',
+        '<i class="fas fa-info"></i><h4>Info</h4><i class="far fa-times-circle"></i>');
+    info.showNotification();
 }
