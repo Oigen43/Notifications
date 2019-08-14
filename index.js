c29508deb0cd62478eb2efe44cff838b47c44ddd
Yauheni Charkes <Yauheni.Charkes@itechart-group.com>
2019-08-14 15:27:30 +0300

    fix: fix toasts inheritance

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
+const ID = 0;
+
 function Notification() {
 }
 
 Notification.prototype.showNotification = function() {
-    var notificationsList = document.querySelector('.section-notifications');
+    var place = document.querySelector(this.place);
     var notification = document.createElement('div');
     notification.className = this.type;
     notification.innerHTML = this.content;
-    notificationsList.appendChild(notification);
-    setTimeout(removeToast, this.time, notification);
+    place.appendChild(notification);
 };
 
 function ToastNotification(type, content) {
     this.type = type;
     this.content = content;
+    this.place = '.section-notifications';
     this.time = 1000; // 5s
 };
 
 ToastNotification.prototype = Object.create(Notification.prototype);
+
+function ModalWindow() {
+    this.content;
+}
+
+ModalWindow.prototype = Object.create(Notification.prototype);
 
 var errorButton = document.querySelector('.error-button');
 var warningButton = document.querySelector('.warning-button');
@@ -28,40 +36,47 @@ errorButton.onclick = function() {
     var error = new ToastNotification('error-notification notification',
         '<i class="fas fa-times"></i><h4>Error</h4><i class="far fa-times-circle"></i>');
     error.showNotification();
+    setTimeout(removeToast, error.time, document.querySelector(error.place).lastChild);
 }
 
 warningButton.onclick = function() {
     var warning = new ToastNotification('warning-notification notification',
         '<i class="fas fa-exclamation-triangle"></i><h4>Warning</h4><i class="far fa-times-circle"></i>');
     warning.showNotification();
+    setTimeout(removeToast, warning.time, document.querySelector(warning.place).lastChild);
 }
 
 successButton.onclick = function() {
     var success = new ToastNotification('success-notification notification',
         '<i class="fas fa-check"></i><h4>Success</h4><i class="far fa-times-circle"></i>');
     success.showNotification();
+    setTimeout(removeToast, success.time, document.querySelector(success.place).lastChild);
 }
 
 infoButton.onclick = function() {
     var info = new ToastNotification('info-notification notification',
         '<i class="fas fa-info"></i><h4>Info</h4><i class="far fa-times-circle"></i>');
     info.showNotification();
+    setTimeout(removeToast, info.time, document.querySelector(info.place).lastChild);
 }
 
 modalButton.onclick = function() {
-    var modal = document.querySelector('.modal');
+    var modal = document.createElement('div');
+    modal.className = 'modal';
     var closeButton = document.querySelector(".close-button");
 
-    function toggleModal() {
-        modal.classList.toggle("show-modal");
-    }
-    toggleModal();
-    window.onclick = function(event) {
-        if (event.target === modal) {
-            toggleModal();
-        }
-    }
-    closeButton.onclick = toggleModal;
+    document.querySelector('main').appendChild(modal);
+
+    // function toggleModal() {
+    //     modal.classList.toggle("show-modal");
+    // }
+    // toggleModal();
+    // window.onclick = function(event) {
+    //     if (event.target === modal) {
+    //         toggleModal();
+    //     }
+    // }
+    // closeButton.onclick = toggleModal;
 }
 
 var toastsSection = document.querySelector('.section-notifications');
